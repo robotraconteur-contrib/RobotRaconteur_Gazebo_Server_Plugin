701d110d6baa640dd3c47791ff987760866f5331
John Wason <wason@wasontech.com>
2020-03-24 22:29:57 -0400

    Bump copyright year

--- gazebo_robotraconteur_server_plugin_examples/scripts/gazebo_forcetorque_client.py
+++ gazebo_robotraconteur_server_plugin_examples/scripts/gazebo_forcetorque_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2016 Wason Technology, LLC
+# Copyright (C) 2016-2020 Wason Technology, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
